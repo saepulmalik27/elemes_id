c08f3419ecf897fdee5231d8e296594ed430672d
maliksaepul <malik@inspigo.id>
2023-05-30 20:41:05 +0700

    feat : setup color config

---
+++ src/config/colors.ts
@@ -0,0 +1,29 @@
+const colors = {
+    transparent: 'transparent',
+    current: 'currentColor',
+    white: '#FFFFFF',
+    'olivegreen' : {
+        lighter : '#F9FFF6',
+        light : '#F0FEEB',
+        DEFAULT : '#8BAC3E',
+    },
+    'tealblue' : {
+        light : "#E6F3F5",
+        DEFAULT : "#40A2B1",
+    },
+    'royalblue' : {
+        light : "#EAEEFA",
+        DEFAULT : "#405EB6",
+    },
+    'deepmagenta' : {
+        light : "#F9EEF3",
+        DEFAULT : "#B640A2",
+    },
+    'limegreen' : {
+        light : "#F3F7D9",
+        DEFAULT : "#A4B441",
+    }
+
+}
+
+module.exports = colors
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('./src/config/colors')
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -6,6 +7,7 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors:colors, 
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
